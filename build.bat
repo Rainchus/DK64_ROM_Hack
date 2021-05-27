138358a41fa3e5287ac38e9b793bc64ec1e2c14d
Rainchus <Rainchus@github.com>
2021-05-26 19:13:27 -0500

    make build.bat generate obj/ for .o files

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+mkdir obj
 mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c src/global.c
 mv global.o obj/
 mips64-elf-gcc -Wall -O1 -mtune=vr4300 -march=vr4300 -mabi=32 -fomit-frame-pointer -G0 -c src/main.c
